682f3faf71c7e1c561fbc0a414c565a99b9735ef
Hasegawa, Raku <raku@exhibition.jp>
2026-02-25 22:32:21 +0900

    exlib: Add ExApp

--- examples/exlib/src/lib.rs
+++ examples/exlib/src/lib.rs
@@ -2,3 +2,23 @@ pub mod components;
 pub mod event;
 pub mod hooks;
 pub mod render;
+pub mod view;
+
+use crate::view::View;
+use zintl::sequence::Arena;
+
+pub struct ExApp<'a> {
+    arena: Arena<'a>,
+    root: Box<dyn View>,
+}
+
+impl<'a> ExApp<'a> {
+    pub fn new(root: impl View) -> Self {
+        ExApp {
+            arena: Arena::new(),
+            root: Box::new(root),
+        }
+    }
+
+    pub fn start() {}
+}
