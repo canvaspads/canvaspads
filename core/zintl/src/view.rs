6cda62cfc914fcd23dcf641228f60764d8c6d64a
Hasegawa, Raku <raku@exhibition.jp>
2026-02-25 22:32:06 +0900

    zintl: View trait default implementation

--- core/zintl/src/view.rs
+++ core/zintl/src/view.rs
@@ -3,6 +3,7 @@ pub struct Context {}
 pub trait View {
     type Output;
 
-    fn init(&mut self, cx: &mut Context);
+    fn init(&mut self, _cx: &mut Context) {}
     fn render(&self, cx: &mut Context) -> Self::Output;
+    fn deinit(&mut self, _cx: &mut Context) {}
 }
